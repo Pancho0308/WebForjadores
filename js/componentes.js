3762c5aebc762abfd9f26b0d1d4c2a9c7a0e84d4
AlejandroVC <alejandrovenanciocalvino@gmail.com>
2026-08-06 22:09:51 +0200

    nav bar actualizado

--- js/componentes.js
+++ js/componentes.js
@@ -20,6 +20,7 @@ function cargarNavbar() {
             <li class="nav-item"><a class="nav-link" href="eventos.html">Eventos</a></li>
             <li class="nav-item"><a class="nav-link" href="que-es-forjaversario.html">Forjaversarios</a></li>
             <li class="nav-item"><a class="nav-link" href="redes.html">Redes</a></li>
+            <li class="nav-item"><a class="nav-link" href="colaboraciones.html">Colaboraciones</a></li>
             <li class="nav-item"><a class="btn btn-donar" href="#">Donar ♡</a></li>
           </ul>
         </div>
